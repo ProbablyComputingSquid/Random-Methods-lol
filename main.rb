0727b8b5054d218b502fc911cd59e6671ae9228a
squids are cool <jwbolling001@gmail.com>
2020-07-20 19:12:56 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,38 @@
+#citation requirments
+#Put my repl.it username in a comment before any copied code.
+#include methods
+
+
+
+
+
+print ("Hi! Welcome to")
+sleep(2)
+puts " the sort of gigantic compliation of a ton of ruby methods so yeah."
+
+#puts a
+#puts a
+puts "Like, i just like, you know, like got a ton of methods."
+
+
+
+
+
+puts "_________________________________________________________________________________"
+
+
+puts "roll_dice function.."
+
+puts "Choose your own numbers!"
+puts "First number!"
+puts "The first number is the number of dice"
+puts "Enter in the number of dice to roll"
+num1 = gets.chomp.to_i
+puts "Enter in the number of sides you want the dice to have."
+num2 = gets.chomp.to_i
+
+puts "Yay! this is your number! " + roll_dice(num1, num2) 
+
+
+
+
